c10a13f63d1eccfeca5c6041ef48a78161a16448
jim9292bb <jim9292bb@gmail.com>
2024-05-13 21:59:41 +0800

    update

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace NotePad
 {
@@ -15,6 +16,38 @@ namespace NotePad
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "選擇檔案";
+            openFileDialog1.Filter = "文字檔案 (*.txt)|*.txt|所有檔案 (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.InitialDirectory = "C:\\";
+            openFileDialog1.Multiselect = true;
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                try
+                {
+                    string selectedFileName = openFileDialog1.FileName;
+                    using (FileStream fileStream = new FileStream(selectedFileName, FileMode.Open, FileAccess.Read))
+                    {
+                        using (StreamReader streamReader = new StreamReader(fileStream, Encoding.UTF8))
+                        {
+                            rtbText.Text = streamReader.ReadToEnd();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("讀取檔案時發生錯誤: " + ex.Message, "錯誤訊息", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("使用者取消了選擇檔案操作。", "訊息", MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation);
+            }
         }
     }
 }
